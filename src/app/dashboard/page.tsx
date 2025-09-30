4fb9d6f98b3374457f192d85588c27d8ea1c16a3
Vihas Ravi Poojari <vihas.poojari45@gmail.com>
2025-10-01 00:02:57 +0530

    refactor: remove mobile detection logic from Dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,16 +12,6 @@ import map from '@/images/map-images.jpg';
 
 const Dashboard = () => {
     const [showMobileBlock, setShowMobileBlock] = useState<boolean>(false);
-    const [isMobile, setIsMobile] = useState<boolean>(false);
-    
-        useEffect(() => {
-            const checkIfMobile = () => {
-                setIsMobile(window.innerWidth <= 768);
-            };
-            checkIfMobile();
-            window.addEventListener('resize', checkIfMobile);
-            return () => window.removeEventListener('resize', checkIfMobile);
-        }, []);
 
   return (
     <div className="min-h-screen bg-gray-100 p-15 pt-25 pb-60 flex justify-center">
